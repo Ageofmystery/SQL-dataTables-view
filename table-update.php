f70f9894ff8fc670542ef802c12ebce5f28c64a7
Ageofmystery <ageofmystery@gmail.com>
2016-04-11 14:39:13 +0300

    Fix ability to search in format d.m.Y

--- table-update.php
+++ table-update.php
@@ -9,12 +9,6 @@ parse_str($_REQUEST['form_content'], $data);
 //declare id
 $data_id = $data['id'];
 
-//convert our data format to sql datetime format
-$fullFormat = explode(" ",$data['datereg']);
-$dateFormat  = explode(".",$fullFormat[0]);
-$timeFormat  = explode(":",$fullFormat[1]);
-$finalDate = $dateFormat[2]."-".$dateFormat[1]."-".$dateFormat[0]." ". $timeFormat[0].":".$timeFormat[1].":".$timeFormat[2];
-
 //begin to construct sql update request by concatinating data
 $sql = "UPDATE `user_datainfo` SET ";
 $sql .= "`first_name` = '" . $data['firstname'] . "', ";
@@ -22,7 +16,7 @@ $sql .= "`last_name` = '" . $data['lastname'] . "', ";
 $sql .= "`birthplace` = '" . $data['birthplace'] . "', ";
 $sql .= "`profession` = '" . $data['profession'] . "', ";
 $sql .= "`phonenumber` = '" . $data['phonenumber'] . "', ";
-$sql .= "`datetime` = '" . $finalDate . "' ";
+$sql .= "`datetime` = '" . $data['datereg'] . "' ";
 $sql .= "WHERE id = '" . $data['id'] . "'";
 
 //test for no empty id
